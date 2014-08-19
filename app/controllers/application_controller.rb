2281e1fd17b81448ab091394d54a039a5106e632
@yepesasecas <yepes07@gmail.com>
2014-08-19 14:24:15 -0500

    null sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: true
 end

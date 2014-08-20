aea1ff3ebe4fe77df1cf3351c502d49df4befc79
@yepesasecas <yepes07@gmail.com>
2014-08-20 12:18:30 -0500

    order text

--- db/migrate/20140819185655_create_notifications.rb
+++ db/migrate/20140819185655_create_notifications.rb
@@ -1,9 +1,10 @@
 class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
-      t.string :status
+      t.string  :status
       t.integer :order_id
-
+      t.text    :order, limit: false
+      t.string  :partner_key
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820145631) do
+ActiveRecord::Schema.define(version: 20140819185655) do
 
   create_table "notifications", force: true do |t|
     t.string   "status"
     t.integer  "order_id"
+    t.text     "order"
+    t.string   "partner_key"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "partner_key"
-    t.string   "order"
   end
 
 end

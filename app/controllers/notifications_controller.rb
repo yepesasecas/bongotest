5c68f10beee52a8bdf39ce2ffc5597c4b954e127
@yepesasecas <yepes07@gmail.com>
2014-08-25 11:58:24 -0500

    add ip validation

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -62,8 +62,12 @@ class NotificationsController < ApplicationController
 
   def callback
     order_id = Notification.decode(params["order"])
-    p request.remote_ip
-    @notification = Notification.create status: params["status"], partner_key: params["partner_key"], order: order_id, ip: "--"
+    if request.remote_ip
+      ip = request.remote_ip 
+    else
+      ip = "Not Identified"
+    end
+    @notification = Notification.create status: params["status"], partner_key: params["partner_key"], order: order_id, ip: ip
     render text: "SUCCESS"
   end
 

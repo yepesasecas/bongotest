3b4e57e0f7e2ce19e93626b816c08d641ab264e6
Andres Yepes <yepes07@gmail.com>
2014-12-11 00:51:21 -0500

    remove routes and controller actions

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,67 +1,12 @@
 class NotificationsController < ApplicationController
-  before_action :set_notification, only: [:show, :edit, :update, :destroy]
-
+  
   def index
     @notifications = Notification.all.order("created_at DESC")
   end
 
-  def show
-  end
-
-  def new
-    @notification = Notification.new
-  end
-
-  def edit
-  end
-
-  def create
-    @notification = Notification.new(notification_params)
-    respond_to do |format|
-      if @notification.save
-        format.html do
-          redirect_to @notification, notice: 'Notification was successfully created.' 
-        end
-        format.json { render :show, status: :created, location: @notification }
-      else
-        format.html { render :new }
-        format.json do
-          render json: @notification.errors, status: :unprocessable_entity 
-        end
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @notification.update(notification_params)
-        format.html do 
-          redirect_to @notification, notice: 'Notification was successfully updated.' 
-        end
-        format.json do
-          render :show, status: :ok, location: @notification 
-        end
-      else
-        format.html { render :edit }
-        format.json do 
-          render json: @notification.errors, status: :unprocessable_entity 
-        end
-      end
-    end
-  end
-
-  def destroy
-    @notification.destroy
-    respond_to do |format|
-      format.html do
-        redirect_to notifications_url,notice: 'Notification was successfully destroyed.' 
-      end
-      format.json { head :no_content }
-    end
-  end
-
   def callback
-    notification = Notification.build_with params
+    notification = Notification.build_with params, request: request
+
     if notification.save!
       render text: "SUCCESS"
     else
@@ -70,20 +15,8 @@ class NotificationsController < ApplicationController
   end
 
   private
-    def set_notification
-      @notification = Notification.find(params[:id])
-    end
 
     def notification_params
       params.require(:notification).permit(:status, :order, :partner_key, :token)
     end
-
-    def get_request_ip
-      if request.remote_ip
-        request.remote_ip
-      else 
-        "Not Identified"
-      end
-    end
-
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,28 +2,17 @@ require "base64"
 
 class Notification < ActiveRecord::Base
 
-  def self.build_with(params = {})
-
-    order      = Base64.decode64(params["order"])
+  def self.build_with(params = {}, request)
+    order      = Base64.decode64(params[:order])
     order_id   = order.partition("idorder>")[2].partition("</idorder")[0]
     token_hash = order.partition("hash>")[2].partition("</hash")[0]
 
     new(order:       order,
         order_id:    order_id,
-        status:      params["status"],
-        partner_key: params["partner_key"],
-        token:       params["token"],
-        token_hash:  token_hash)
+        status:      params[:status],
+        partner_key: params[:partner_key],
+        token:       params[:token],
+        token_hash:  token_hash,
+        ip:          request[:request].remote_ip)
   end
-
-  # def self.decode(order)
-  #   Base64.decode64 order
-  # end
-
-  # def self.xml_id(order)
-  #   decode = self.decode order
-  #   order_id = decode.partition("idorder>")[2].partition("</idorder")[0]
-  #   order_id
-  # end
-
 end

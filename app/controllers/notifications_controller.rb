5839e943aa67bc21dcd691f7bc70ed4bdc136a01
Andres Yepes <yepes07@gmail.com>
2015-10-06 22:15:57 -0500

    ignore .DS_Store File and add try/Catch Exception Handler

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,16 +1,20 @@
 class NotificationsController < ApplicationController
-  
+
   def index
     @notifications = Notification.all.order("created_at DESC")
   end
 
   def callback
-    notification = Notification.build_with params, request: request
+    begin
+      notification = Notification.build_with params, request: request
 
-    if notification.save!
-      render text: "SUCCESS"
-    else
-      render text: "PAILA! No se pudo guardar la notification. :("
+      if notification.save!
+        render text: "SUCCESS"
+      else
+        render text: "PAILA! No se pudo guardar la notification. :("
+      end
+    rescue
+      render text: "ERROR! No se pudo guardar la notification. :("
     end
   end
 

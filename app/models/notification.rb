4b17596764e721bc66d2117c0be9a6be1a82cb71
Andres Yepes <yepes07@gmail.com>
2018-04-02 13:50:18 -0500

    ignore exceptions

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,8 +4,8 @@ class Notification < ActiveRecord::Base
 
   def self.build_with(params = {}, request)
     order      = Base64.decode64(params[:order])
-    order_id   = order.partition("idorder>")[2].partition("</idorder")[0]
-    token_hash = order.partition("hash>")[2].partition("</hash")[0]
+    order_id   = order.partition("idorder>")[2].partition("</idorder")[0] || "n/a"
+    token_hash = order.partition("hash>")[2].partition("</hash")[0] || "n/a"
 
     new(order:       order,
         order_id:    order_id,

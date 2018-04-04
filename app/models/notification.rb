87d829334a39612f86c0524316f7dd36eb58635e
Andres Yepes <yepes07@gmail.com>
2018-04-03 21:55:03 -0500

    add product notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,19 +3,30 @@ require "base64"
 class Notification < ActiveRecord::Base
 
   def self.build_with(params = {}, request)
-    # order      = Base64.decode64(params[:order])
-    order = params[:order]
-    # order_id   = order.partition("idorder>")[2].partition("</idorder")[0] || "n/a"
-    order_id = "n/a"
-    # token_hash = order.partition("hash>")[2].partition("</hash")[0] || "n/a"
-    token_hash = "n/a"
+    partner_key = params[:partner_key]
+    token       = params[:token]
+    ip          = request[:request].remote_ip
+
+    if params[:productID]
+      order      = params[:order]
+      order_id   = params[:productID]
+      token_hash = "n/a"
+      status     = "Product"
+    else
+      order      = Base64.decode64(params[:order])
+      order_id   = order.partition("idorder>")[2].partition("</idorder")[0] || "n/a"
+      token_hash = order.partition("hash>")[2].partition("</hash")[0] || "n/a"
+      status     = params[:status]
+    end
+
+    puts params
 
     new(order:       order,
         order_id:    order_id,
-        status:      params[:status],
-        partner_key: params[:partner_key],
-        token:       params[:token],
+        status:      status,
+        partner_key: partner_key,
+        token:       token,
         token_hash:  token_hash,
-        ip:          request[:request].remote_ip)
+        ip:          ip)
   end
 end

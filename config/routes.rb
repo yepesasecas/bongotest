3b29aae674ed9dba79a54384ab21d01638448b73
Andres Yepes <yepes07@gmail.com>
2018-05-03 10:04:55 -0500

    root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :notifications, only: [:index] do
     post 'callback', on: :collection
-    get 'callback', to: redirect("/")
+    get 'callback', to: redirect(root_path)
   end
 
   root 'notifications#index'
